a2c48c72035e93d06412676e447b884f1ad7e84c
Eracoy <thebluefire39@gmail.com>
2021-05-20 18:13:19 -0400

    Initialize random grid of colors

--- global_potential_annealing/potential_based.py
+++ global_potential_annealing/potential_based.py
@@ -1,4 +1,16 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
-all_intensities = np.linspace(0,1,256)
-r,g,b = np.array(np.meshgrid(all_intensities, all_intensities, all_intensities)).T.reshape(-1,3)
+def potential(grid):
+    
+
+color_depth = 16
+all_intensities = np.linspace(0, 1, color_depth)
+all_colors = np.array(np.meshgrid(all_intensities, all_intensities, all_intensities)).T.reshape(-1,3)
+# np.random.shuffle(all_colors)
+grid = all_colors.reshape(-1,np.int(color_depth*np.sqrt(color_depth)),3)
+print(grid[0,0])
+plt.imshow(grid)
+plt.show()
+plt.pause(0.001)
+
